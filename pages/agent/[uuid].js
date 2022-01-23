3d3b8473a696194ebee5a2f9e0eee8fdef2b22c2
Varad Gundap <varad615@gmail.com>
2022-01-23 12:23:48 +0530

    fixed

--- pages/agent/[uuid].js
+++ pages/agent/[uuid].js
@@ -55,17 +55,17 @@ const Details = ({ mydata }) => {
         </Head>
         <div>
           <div>
-            <section class="text-[#0F1822] body-font overflow-hidden">
-              <div class="container px-5 py-10 mx-auto">
-                <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <section className="text-[#0F1822] body-font overflow-hidden">
+              <div className="container px-5 py-10 mx-auto">
+                <div className="lg:w-4/5 mx-auto flex flex-wrap">
                   <img
                     alt="ecommerce"
-                    class="lg:w-1/2 w-full lg:h-auto object-cover object-center rounded"
+                    className="lg:w-1/2 w-full lg:h-auto object-cover object-center rounded"
                     src={mydata.fullPortrait}
                   />
-                  <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 lg:mt-0">
+                  <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 lg:mt-0">
                     <div className="bg-[#FF4654] rounded-lg p-5">
-                      <h1 class="text-[#0F1822] text-3xl title-font font-medium mb-1 flex flex-row">
+                      <h1 className="text-[#0F1822] text-3xl title-font font-medium mb-1 flex flex-row">
                         <img src={mydata.displayIcon} className="h-10" /> &nbsp;{" "}
                         {mydata.displayName}
                       </h1>
@@ -76,7 +76,7 @@ const Details = ({ mydata }) => {
                           &nbsp; {mydata.role.displayName}
                         </div>
                       </p>
-                      <p class="leading-relaxed text-white">
+                      <p className="leading-relaxed text-white">
                         {mydata.description}
                       </p>
                     </div>
--- pages/version.js
+++ pages/version.js
@@ -55,59 +55,59 @@ const version = ({ mydata }) => {
           </div>
         </div>
         <div>
-          <section class="text-gray-600 body-font">
-            <div class="container px-5 py-24 mx-auto flex flex-wrap">
-              <div class="flex flex-wrap w-full">
-                <div class="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
-                  <div class="flex relative pb-12">
-                    <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                      <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto flex flex-wrap">
+              <div className="flex flex-wrap w-full">
+                <div className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
+                  <div className="flex relative pb-12">
+                    <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                     </div>
-                    <div class="flex-shrink-0 w-10 h-10 rounded-full bg-[#FB4554] hover:bg-[#0F1822] transition hover:border-2 hover:border-[#FB4554] inline-flex items-center justify-center relative z-10"></div>
-                    <div class="flex-grow pl-4">
-                      <h2 class="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                    <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#FB4554] hover:bg-[#0F1822] transition hover:border-2 hover:border-[#FB4554] inline-flex items-center justify-center relative z-10"></div>
+                    <div className="flex-grow pl-4">
+                      <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
                         Version
                       </h2>
-                      <p class="leading-relaxed">{mydata.version}</p>
+                      <p className="leading-relaxed">{mydata.version}</p>
                     </div>
                   </div>
-                  <div class="flex relative pb-12">
-                    <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                      <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  <div className="flex relative pb-12">
+                    <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                     </div>
-                    <div class="flex-shrink-0 w-10 h-10 rounded-full bg-[#FB4554] hover:bg-[#0F1822] transition hover:border-2 hover:border-[#FB4554] inline-flex items-center justify-center text-white relative z-10"></div>
-                    <div class="flex-grow pl-4">
-                      <h2 class="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                    <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#FB4554] hover:bg-[#0F1822] transition hover:border-2 hover:border-[#FB4554] inline-flex items-center justify-center text-white relative z-10"></div>
+                    <div className="flex-grow pl-4">
+                      <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
                         Build Version
                       </h2>
-                      <p class="leading-relaxed">{mydata.buildVersion}</p>
+                      <p className="leading-relaxed">{mydata.buildVersion}</p>
                     </div>
                   </div>
-                  <div class="flex relative pb-12">
-                    <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                      <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  <div className="flex relative pb-12">
+                    <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                      <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                     </div>
-                    <div class="flex-shrink-0 w-10 h-10 rounded-full bg-[#FB4554] hover:bg-[#0F1822] transition hover:border-2 hover:border-[#FB4554] inline-flex items-center justify-center text-white relative z-10"></div>
-                    <div class="flex-grow pl-4">
-                      <h2 class="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                    <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#FB4554] hover:bg-[#0F1822] transition hover:border-2 hover:border-[#FB4554] inline-flex items-center justify-center text-white relative z-10"></div>
+                    <div className="flex-grow pl-4">
+                      <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
                         Riot Client Version
                       </h2>
-                      <p class="leading-relaxed">{mydata.riotClientVersion}</p>
+                      <p className="leading-relaxed">{mydata.riotClientVersion}</p>
                     </div>
                   </div>
-                  <div class="flex relative pb-12">
-                    <div class="h-full w-10 absolute inset-0 flex items-center justify-center"></div>
-                    <div class="flex-shrink-0 w-10 h-10 rounded-full bg-[#FB4554] hover:bg-[#0F1822] transition hover:border-2 hover:border-[#FB4554] inline-flex items-center justify-center text-white relative z-10"></div>
-                    <div class="flex-grow pl-4">
-                      <h2 class="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                  <div className="flex relative pb-12">
+                    <div className="h-full w-10 absolute inset-0 flex items-center justify-center"></div>
+                    <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#FB4554] hover:bg-[#0F1822] transition hover:border-2 hover:border-[#FB4554] inline-flex items-center justify-center text-white relative z-10"></div>
+                    <div className="flex-grow pl-4">
+                      <h2 className="font-medium title-font text-sm text-white mb-1 tracking-wider">
                         Build Date
                       </h2>
-                      <p class="leading-relaxed">{mydata.buildDate}</p>
+                      <p className="leading-relaxed">{mydata.buildDate}</p>
                     </div>
                   </div>
                 </div>
                 <img
-                  class="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
+                  className="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
                   src="https://media.valorant-api.com/bundles/d958b181-4e7b-dc60-7c3c-e3a3a376a8d2/displayicon.png"
                   alt="step"
                 />
